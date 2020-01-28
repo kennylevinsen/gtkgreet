bae52043cb3ea53298451592091d4dd19ceae38f
Kenny Levinsen <kl@kl.wtf>
2020-01-28 01:47:51 +0100

    WIP

--- proto.c
+++ proto.c
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <errno.h>
 
+#include "proto.h"
+#include "window.h"
+
 struct header {
     uint32_t magic;
     uint32_t version;
@@ -16,19 +19,14 @@ struct header {
 
 static int write_req(int fd, struct json_object* req) {
     const char* reqstr = json_object_get_string(req);
-    struct header header = {
-        .magic = 0xAFBFCFDF,
-        .version = 1,
-        .payload_len = strlen(reqstr),
-    };
-
-    if (write(fd, &header, sizeof(struct header)) != sizeof(struct header)) {
+    uint32_t len = strlen(reqstr);
+    if (write(fd, &len, 4) != 4) {
         return -1;
     }
 
     ssize_t off = 0;
-    while (off < header.payload_len) {
-        ssize_t n = write(fd, &reqstr[off], header.payload_len-off);
+    while (off < len) {
+        ssize_t n = write(fd, &reqstr[off], len-off);
         if (n < 0) {
             return -1;
         }
@@ -39,30 +37,24 @@ static int write_req(int fd, struct json_object* req) {
 }
 
 static struct json_object* read_resp(int fd) {
-    struct header header = {};
     struct json_object* resp = NULL;
     char *respstr = NULL;
+    uint32_t len;
     ssize_t off = 0;
 
-    while (off < sizeof(struct header)) {
-        char* headerp = (char*)&header;
-        ssize_t n = read(fd, &headerp[off], sizeof(struct header)-off);
+    while (off < 4) {
+        char* headerp = (char*)&len;
+        ssize_t n = read(fd, &headerp[off], 4-off);
         if (n < 0) {
             goto end;
         }
         off += n;
     }
 
-
-    if (header.magic != 0xAFBFCFDF ||
-        header.version != 1) {
-        goto end;
-    }
-
     off = 0;
-    respstr = (char*)calloc(1, header.payload_len+1);
-    while (off < header.payload_len) {
-        int n = read(fd, &respstr[off], header.payload_len-off);
+    respstr = (char*)calloc(1,len+1);
+    while (off <len) {
+        int n = read(fd, &respstr[off],len-off);
         if (n < 0) {
             goto end;
         }
@@ -128,6 +120,180 @@ end:
         close(fd);
     }
     return resp;
+}
+
+struct proto_status send_create_session(const char *username) {
+    struct json_object* req = json_object_new_object();
+    json_object_object_add(req, "type", json_object_new_string("create_session"));
+    json_object_object_add(req, "username", json_object_new_string(username));
+    struct json_object* resp = roundtrip(req);
+
+    struct proto_status ret;
+
+    struct json_object* type = json_object_object_get(resp, "type");
+    const char* typestr = json_object_get_string(type);
+    if (typestr == NULL || strcmp(typestr, "error") == 0) {
+        ret.success = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "success") == 0) {
+        ret.success = 1;
+        ret.has_questions = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "auth_question") == 0) {
+        ret.success = 1;
+        ret.has_questions = 1;
+        struct json_object *style = json_object_object_get(resp, "style");
+        const char* stylestr = json_object_get_string(style);
+        if (strcmp(stylestr, "visible") == 0) {
+            ret.question_type = QuestionTypeVisible;
+        } else if (strcmp(stylestr, "secret") == 0) {
+            ret.question_type = QuestionTypeSecret;
+        } else if (strcmp(stylestr, "info") == 0) {
+            ret.question_type = QuestionTypeInfo;
+        } else if (strcmp(stylestr, "error") == 0) {
+            ret.question_type = QuestionTypeError;
+        } else {
+            ret.success = 0;
+            goto done;
+        }
+
+        struct json_object *question = json_object_object_get(resp, "question");
+        const char* questionstr = json_object_get_string(question);
+        strncpy(ret.question, questionstr, 63);
+        goto done;
+    }
+
+    ret.success = 0;
+
+done:
+    json_object_put(resp);
+    return ret;
+}
+
+struct proto_status send_auth_answer(const char *answer) {
+    struct json_object* req = json_object_new_object();
+    json_object_object_add(req, "type", json_object_new_string("answer_auth_question"));
+    json_object_object_add(req, "answer", json_object_new_string(answer));
+    struct json_object* resp = roundtrip(req);
+
+    struct proto_status ret;
+
+    struct json_object* type = json_object_object_get(resp, "type");
+    const char* typestr = json_object_get_string(type);
+    if (typestr == NULL || strcmp(typestr, "error") == 0) {
+        ret.success = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "success") == 0) {
+        ret.success = 1;
+        ret.has_questions = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "auth_question") == 0) {
+        ret.success = 1;
+        ret.has_questions = 1;
+        struct json_object *style = json_object_object_get(resp, "style");
+        const char* stylestr = json_object_get_string(style);
+        if (strcmp(stylestr, "visible") == 0) {
+            ret.question_type = QuestionTypeVisible;
+        } else if (strcmp(stylestr, "secret") == 0) {
+            ret.question_type = QuestionTypeSecret;
+        } else if (strcmp(stylestr, "info") == 0) {
+            ret.question_type = QuestionTypeInfo;
+        } else if (strcmp(stylestr, "error") == 0) {
+            ret.question_type = QuestionTypeError;
+        } else {
+            ret.success = 0;
+            goto done;
+        }
+
+        struct json_object *question = json_object_object_get(resp, "question");
+        const char* questionstr = json_object_get_string(question);
+        strncpy(ret.question, questionstr, 63);
+        goto done;
+    }
+
+    ret.success = 0;
+
+done:
+    json_object_put(resp);
+    return ret;
+}
+
+
+struct proto_status send_start_session(const char *command) {
+    struct json_object* req = json_object_new_object();
+    json_object_object_add(req, "type", json_object_new_string("start_session"));
+
+    struct json_object* cmd = json_object_new_array();
+    json_object_array_add(cmd, json_object_new_string(command));
+    json_object_object_add(req, "command", cmd);
+
+    struct json_object* env = json_object_new_array();
+
+    char buf[128];
+    snprintf(buf, 128, "XDG_SESSION_DESKTOP=%s", command);
+    json_object_array_add(env, json_object_new_string(buf));
+    snprintf(buf, 128, "XDG_CURRENT_DESKTOP=%s", command);
+    json_object_array_add(env, json_object_new_string(buf));
+
+    json_object_object_add(req, "env", env);
+
+    struct json_object* resp = roundtrip(req);
+
+    struct proto_status ret;
+
+    struct json_object* type = json_object_object_get(resp, "type");
+    const char* typestr = json_object_get_string(type);
+    if (typestr == NULL || strcmp(typestr, "error") == 0) {
+        ret.success = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "success") == 0) {
+        ret.success = 1;
+        ret.has_questions = 0;
+        goto done;
+    }
+
+    ret.success = 0;
+
+done:
+    json_object_put(resp);
+    return ret;
+}
+
+struct proto_status send_cancel_session() {
+    struct json_object* req = json_object_new_object();
+    json_object_object_add(req, "type", json_object_new_string("cancel_session"));
+    struct json_object* resp = roundtrip(req);
+
+    struct proto_status ret;
+
+    struct json_object* type = json_object_object_get(resp, "type");
+    const char* typestr = json_object_get_string(type);
+    if (typestr == NULL || strcmp(typestr, "error") == 0) {
+        ret.success = 0;
+        goto done;
+    }
+
+    if (strcmp(typestr, "success") == 0) {
+        ret.success = 1;
+        ret.has_questions = 0;
+        goto done;
+    }
+
+    ret.success = 0;
+
+done:
+    json_object_put(resp);
+    return ret;
 }
 
 int send_login(const char *username, const char * password, const char *command) {
--- proto.h
+++ proto.h
@@ -1,6 +1,20 @@
 #ifndef _PROTO_H
 #define _PROTO_H
 
+#include "window.h"
+
+struct proto_status {
+    int success;
+    int has_questions;
+    enum QuestionType question_type;
+    char question[64];
+};
+
+struct proto_status send_create_session(const char *username);
+struct proto_status send_start_session(const char *username);
+struct proto_status send_auth_answer(const char *answer);
+struct proto_status send_cancel_session();
+
 int send_login(const char *username, const char * password, const char *command);
 int send_shutdown(const char *action);
 
