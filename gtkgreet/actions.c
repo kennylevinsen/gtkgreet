cb075bd5a8d2a155252f9e7c20bdbefdf68f10e3
Kenny Levinsen <kl@kl.wtf>
2020-03-30 04:01:11 +0200

    Minor refactor of action_answer_question

--- gtkgreet/actions.c
+++ gtkgreet/actions.c
@@ -8,9 +8,60 @@
 #include "gtkgreet.h"
 #include "window.h"
 
+static void handle_response(struct response resp, int start_req) {
+    switch (resp.response_type) {
+        case response_type_success: {
+            if (start_req) {
+                exit(0);
+            }
+            struct request req = {
+                .request_type = request_type_start_session,
+            };
+            strncpy(req.body.request_start_session.cmd, gtkgreet->selected_command, 127);
+            handle_response(roundtrip(req), 1);
+            break;
+        }
+        case response_type_auth_message: {
+            if (start_req) {
+                struct request req = {
+                    .request_type = request_type_cancel_session,
+                };
+                roundtrip(req);
+
+                char *error = "Unexpected auth question";
+                gtkgreet_setup_question(gtkgreet, QuestionTypeInitial, INITIAL_QUESTION, error);
+                break;
+            }
+
+            gtkgreet_setup_question(gtkgreet,
+                (enum QuestionType)resp.body.response_auth_message.auth_message_type,
+                resp.body.response_auth_message.auth_message,
+                NULL);
+            break;
+        }
+        case response_type_roundtrip_error:
+        case response_type_error: {
+            struct request req = {
+                .request_type = request_type_cancel_session,
+            };
+            roundtrip(req);
+
+            char* error = NULL;
+            if (resp.response_type == response_type_error) {
+                if (resp.body.response_error.error_type == error_type_auth) {
+                    error = "Login failed";
+                } else {
+                    error = resp.body.response_error.description;
+                }
+            }
+            gtkgreet_setup_question(gtkgreet, QuestionTypeInitial, INITIAL_QUESTION, error);
+            break;
+        }
+    }
+}
+
 void action_answer_question(GtkWidget *widget, gpointer data) {
     struct Window *ctx = data;
-    struct response resp;
     switch (gtkgreet->question_type) {
         case QuestionTypeInitial: {
             if (gtkgreet->selected_command) {
@@ -25,7 +76,7 @@ void action_answer_question(GtkWidget *widget, gpointer data) {
             if (ctx->input_field != NULL) {
                 strncpy(req.body.request_create_session.username, gtk_entry_get_text((GtkEntry*)ctx->input_field), 127);
             }
-            resp = roundtrip(req);
+            handle_response(roundtrip(req), 0);
             break;
         }
         case QuestionTypeSecret:
@@ -36,7 +87,7 @@ void action_answer_question(GtkWidget *widget, gpointer data) {
             if (ctx->input_field != NULL) {
                 strncpy(req.body.request_post_auth_message_response.response, gtk_entry_get_text((GtkEntry*)ctx->input_field), 127);
             }
-            resp = roundtrip(req);
+            handle_response(roundtrip(req), 0);
             break;
         }
         case QuestionTypeInfo:
@@ -45,58 +96,7 @@ void action_answer_question(GtkWidget *widget, gpointer data) {
                 .request_type = request_type_post_auth_message_response,
             };
             req.body.request_post_auth_message_response.response[0] = '\0';
-            resp = roundtrip(req);
-            break;
-        }
-    }
-
-    switch (resp.response_type) {
-        case response_type_success: {
-            struct request req = {
-                .request_type = request_type_start_session,
-            };
-            strncpy(req.body.request_start_session.cmd, gtkgreet->selected_command, 127);
-            resp = roundtrip(req);
-            if (resp.response_type == response_type_success) {
-                exit(0);
-            }
-            req.request_type = request_type_cancel_session,
-            resp = roundtrip(req);
-
-            char* error = NULL;
-            if (resp.response_type == response_type_error) {
-                if (resp.body.response_error.error_type == error_type_auth) {
-                    error = "Login failed";
-                } else {
-                    error = resp.body.response_error.description;
-                }
-            }
-            gtkgreet_setup_question(gtkgreet, QuestionTypeInitial, INITIAL_QUESTION, error);
-            break;
-        }
-        case response_type_auth_message: {
-            gtkgreet_setup_question(gtkgreet,
-                (enum QuestionType)resp.body.response_auth_message.auth_message_type,
-                resp.body.response_auth_message.auth_message,
-                NULL);
-            break;
-        }
-        case response_type_roundtrip_error:
-        case response_type_error: {
-            struct request req = {
-                .request_type = request_type_cancel_session,
-            };
-
-            char* error = NULL;
-            if (resp.response_type == response_type_error) {
-                if (resp.body.response_error.error_type == error_type_auth) {
-                    error = "Login failed";
-                } else {
-                    error = resp.body.response_error.description;
-                }
-            }
-            roundtrip(req);
-            gtkgreet_setup_question(gtkgreet, QuestionTypeInitial, INITIAL_QUESTION, error);
+            handle_response(roundtrip(req), 0);
             break;
         }
     }
